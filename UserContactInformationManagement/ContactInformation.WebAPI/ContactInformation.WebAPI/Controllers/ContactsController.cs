946aa11f9c096300e90aeddc1a0534163c1b32fd
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-07-28 09:20:24 +0800

    Add project

---
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Controllers/ContactsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactInformation.WebAPI.Controllers
+{
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        public JsonResult GetContacts()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {Id = 1, Firstname = "Charis Arlie", Lastname = "Baclayon"},
+                    new {Id = 1, Firstname = "Leonel Christie", Lastname = "Baclayon"},
+                });
+        }
+    }
+}
