4f87ec34ee5ea2927bd34a36867f369cb52bf81d
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-08 16:47:53 +0800

    Add required attributes in UserRegistrationDto

--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Dtos/User/UserRegistrationDto.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Dtos/User/UserRegistrationDto.cs
@@ -1,10 +1,19 @@
-﻿namespace ContactInformation.WebAPI.Dtos.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactInformation.WebAPI.Dtos.User
 {
     public class UserRegistrationDto
     {
+        [Required(ErrorMessage = "First Name is required.")]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last Name is required.")]
         public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Username is required.")]
         public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; } = string.Empty;
 
     }
