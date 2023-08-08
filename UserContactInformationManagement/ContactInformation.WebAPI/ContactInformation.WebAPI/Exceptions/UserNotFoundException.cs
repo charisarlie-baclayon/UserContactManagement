3f193ace27f0c01fc49f3519f134fd555705ad5a
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-08 23:19:45 +0800

    Add new custom Exceptions

---
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace ContactInformation.WebAPI.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Exceptions/UserUpdateFailedException.cs
@@ -0,0 +1,7 @@
+﻿namespace ContactInformation.WebAPI.Exceptions
+{
+    public class UserUpdateFailedException : Exception
+    {
+        public UserUpdateFailedException(string message) : base(message) { }
+    }
+}
