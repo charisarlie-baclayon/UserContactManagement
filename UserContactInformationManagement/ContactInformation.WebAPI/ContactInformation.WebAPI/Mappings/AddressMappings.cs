e3fddc2a9805c50b257cfdfaf2346c2999a85035
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-09 00:42:21 +0800

    Add new AddressMappings

---
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Mappings/AddressMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ContactInformation.WebAPI.Dtos.Address;
+using ContactInformation.WebAPI.Models;
+
+namespace ContactInformation.WebAPI.Mappings
+{
+    public class AddressMappings : Profile
+    {
+        public AddressMappings()
+        {
+            CreateMap<Address, AddressDto>();
+            CreateMap<Address, AddressCreationDto>();
+            CreateMap<AddressCreationDto, Address>();
+            CreateMap<AddressDto, Address>();
+        }
+    }
+}
