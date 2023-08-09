173f214d9b83ce1436a15eafc8badb8a1f33e4bb
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-09 23:17:25 +0800

    Add xml documentation in address dtos

--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Dtos/Address/AddressCreationDto.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Dtos/Address/AddressCreationDto.cs
@@ -2,11 +2,20 @@
 
 namespace ContactInformation.WebAPI.Dtos.Address
 {
+    /// <summary>
+    /// Data transfer object for creating a new address.
+    /// </summary>
     public class AddressCreationDto
     {
+        /// <summary>
+        /// Gets or sets the description of the address.
+        /// </summary>
         [Required(ErrorMessage = "Address is required.")]
         public string AddressDescription { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Gets or sets the type of the address.
+        /// </summary>
         [Required(ErrorMessage = "Address Type is required.")]
         public string AddressType { get; set; } = string.Empty;
     }
--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Dtos/Address/AddressDto.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Dtos/Address/AddressDto.cs
@@ -1,8 +1,18 @@
 ﻿namespace ContactInformation.WebAPI.Dtos.Address
 {
+    /// <summary>
+    /// Data transfer object for viewing address details.
+    /// </summary>
     public class AddressDto
     {
+        /// <summary>
+        /// Gets or sets the description of the address.
+        /// </summary>
         public string AddressDescription { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the type of the address.
+        /// </summary>
         public string AddressType { get; set; } = string.Empty;
     }
 }
