4033b519f9d601f083dddce64d121c9b86c210f3
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-09 21:25:11 +0800

    Add xml documentation in Contact model

--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Models/Contact.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Models/Contact.cs
@@ -3,22 +3,64 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ContactInformation.WebAPI.Models
 {
+    /// <summary>
+    /// Represents a contact entity in the system.
+    /// </summary>
     [Table("Contacts")]
     public class Contact
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the contact.
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name of the contact.
+        /// </summary>
         public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the last name of the contact.
+        /// </summary>
         public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the list of addresses associated with the contact.
+        /// </summary>
         public List<Address> Addresses { get; set; }
             = new List<Address>();
+
+        /// <summary>
+        /// Gets or sets the phone number of the contact.
+        /// </summary>
         public string PhoneNumber { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the email address of the contact.
+        /// </summary>
         public string EmailAddress { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the birth date of the contact.
+        /// </summary>
         public DateTime BirthDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the contact is a favorite.
+        /// </summary>
         public bool Favorite { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user associated with the contact.
+        /// </summary>
         [ForeignKey("UserId")]
         public User? User { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the user associated with the contact.
+        /// </summary>
         public int UserId { get; set; }
     }
 }
