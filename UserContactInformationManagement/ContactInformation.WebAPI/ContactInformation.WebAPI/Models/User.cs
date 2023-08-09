39116658a0ac99f1ba6b66bb38c16926f1310ff7
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-09 21:20:07 +0800

    Add xml documentation in User model

--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Models/User.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Models/User.cs
@@ -3,18 +3,47 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ContactInformation.WebAPI.Models
 {
+    /// <summary>
+    /// Represents a user entity in the system.
+    /// </summary>
     [Table("Users")]
     public class User
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the user.
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name of the user.
+        /// </summary>
         public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the last name of the user.
+        /// </summary>
         public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
         public string Username { get; set; } = string.Empty;
-        //public string Password { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the password hash for the user.
+        /// </summary>
         public byte[] PasswordHash { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password salt for the user.
+        /// </summary>
         public byte[] PasswordSalt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of contacts associated with the user.
+        /// </summary>
         public List<Contact> Contacts { get; set; } 
             = new List<Contact>();
 
