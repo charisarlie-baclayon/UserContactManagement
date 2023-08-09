32a5f4a3ce6986ec58c1006ce90bbca50a29367e
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-09 20:37:23 +0800

    Add xml documentation in AddressService

--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Services/AddressService/AddressService.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Services/AddressService/AddressService.cs
@@ -6,16 +6,26 @@ using ContactInformation.WebAPI.Repositories.AddressRepository;
 
 namespace ContactInformation.WebAPI.Services.AddressService
 {
+    /// <summary>
+    /// Provides method implementation from IAddressService.
+    /// </summary>
     public class AddressService : IAddressService
     {
         private readonly IMapper _mapper;
         private readonly IAddressRepository _addressRepository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddressService"/> class.
+        /// </summary>
+        /// <param name="mapper">The AutoMapper instance for mapping between entities and DTOs.</param>
+        /// <param name="addressRepository">The repository for address data operations.</param>
         public AddressService(IMapper mapper, IAddressRepository addressRepository)
         {
             _mapper = mapper;
             _addressRepository = addressRepository;
         }
+
+        /// <inheritdoc />
         public async Task<int> CreateAddress(int contactId, AddressCreationDto addressToAdd)
         {
             var newAddress = _mapper.Map<Address>(addressToAdd);
@@ -29,6 +39,7 @@ namespace ContactInformation.WebAPI.Services.AddressService
             return addressId;
         }
 
+        /// <inheritdoc />
         public async Task<bool> DeleteAddress(int contactId, int addressId)
         {
             var deleted = await _addressRepository.DeleteAddress(contactId, addressId);
@@ -39,6 +50,7 @@ namespace ContactInformation.WebAPI.Services.AddressService
             return deleted;
         }
 
+        /// <inheritdoc />
         public async Task<AddressDto> GetAddress(int contactId, int addressId)
         {
             var address = await _addressRepository.GetAddress(contactId, addressId);
@@ -50,6 +62,7 @@ namespace ContactInformation.WebAPI.Services.AddressService
             return newAddressDto;
         }
 
+        /// <inheritdoc />
         public async Task<IEnumerable<AddressDto>> GetAddresses(int contactId)
         {
             var addresses = await _addressRepository.GetAddresses(contactId);
@@ -57,6 +70,7 @@ namespace ContactInformation.WebAPI.Services.AddressService
             return addressDtos;
         }
 
+        /// <inheritdoc />
         public async Task<Address> UpdateAddress(int contactId, int addressId, AddressCreationDto addressToUpdate)
         {
             var existingAddress = await _addressRepository.GetAddress(contactId, addressId);
