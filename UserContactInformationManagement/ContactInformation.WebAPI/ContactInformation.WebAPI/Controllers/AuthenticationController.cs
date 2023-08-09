caa7c3448403fb6044e03e634f50522a1cefc31e
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-09 23:58:05 +0800

    Add xml documentation in AuthenticationControlle

--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Controllers/AuthenticationController.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Controllers/AuthenticationController.cs
@@ -1,16 +1,15 @@
 ﻿using ContactInformation.WebAPI.Dtos.User;
 using ContactInformation.WebAPI.Exceptions;
-using ContactInformation.WebAPI.Models;
 using ContactInformation.WebAPI.Services.AuthenticationService;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
+using IAuthenticationService = ContactInformation.WebAPI.Services.AuthenticationService.IAuthenticationService;
 
 namespace ContactInformation.WebAPI.Controllers
 {
+    /// <summary>
+    /// Controller responsible for user authentication.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class AuthenticationController : ControllerBase
@@ -18,13 +17,43 @@ namespace ContactInformation.WebAPI.Controllers
         private readonly ILogger<AuthenticationController> _logger;
         private readonly IAuthenticationService _authenticationService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthenticationController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger instance for logging.</param>
+        /// <param name="authenticationService">The service for user authentication.</param>
         public AuthenticationController(ILogger<AuthenticationController> logger, IAuthenticationService authenticationService)
         {
             _logger = logger;
             _authenticationService = authenticationService;
         }
 
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        /// <param name="userRegistrationDto">The user registration details.</param>
+        /// <returns>The newly registered user.</returns>
+        /// <remarks>
+        /// Sample Request:
+        /// 
+        ///     POST /api/authentication/register
+        ///     {
+        ///         "username": "newUser",
+        ///         "password": "password123"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Returns the newly registered user.</response>
+        /// <response code="400">Invalid user registration details provided.</response>
+        /// <response code="409">User already exists.</response>
+        /// <response code="500">Internal server error.</response>
         [HttpPost("register")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Register(UserRegistrationDto userRegistrationDto)
         {
             try
@@ -49,7 +78,30 @@ namespace ContactInformation.WebAPI.Controllers
             }
         }
 
+        /// <summary>
+        /// Logs in a user.
+        /// </summary>
+        /// <param name="userLoginDto">The user login details.</param>
+        /// <returns>The authentication token upon successful login.</returns>
+        /// <remarks>
+        /// Sample Request:
+        /// 
+        ///     POST /api/authentication/login
+        ///     {
+        ///         "username": "existinguser",
+        ///         "password": "password123"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Returns the authentication token upon successful login.</response>
+        /// <response code="401">Unauthorized due to invalid credentials.</response>
+        /// <response code="403">Unauthorized access.</response>
+        /// <response code="500">Internal server error.</response>
         [HttpPost("login")]
+        [ProducesResponseType(typeof(AuthenticationToken), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Login(UserLoginDto userLoginDto)
         {
             try
