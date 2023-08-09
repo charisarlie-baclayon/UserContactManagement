463cb14179f2b9c5d997e8b943882ba48038f1dd
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-09 21:26:20 +0800

    Add xml documentation in AddressType enum

--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Models/AddressType.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Models/AddressType.cs
@@ -1,10 +1,28 @@
 ﻿namespace ContactInformation.WebAPI.Models
 {
+    /// <summary>
+    /// Represents the types of addresses.
+    /// </summary>
     public enum AddressType
     {
+        /// <summary>
+        /// Represents a delivery address type.
+        /// </summary>
         Delivery,
+
+        /// <summary>
+        /// Represents a billing address type.
+        /// </summary>
         Billing,
+
+        /// <summary>
+        /// Represents a home address type.
+        /// </summary>
         Home,
+
+        /// <summary>
+        /// Represents a work address type.
+        /// </summary>
         Work
     }
 }
