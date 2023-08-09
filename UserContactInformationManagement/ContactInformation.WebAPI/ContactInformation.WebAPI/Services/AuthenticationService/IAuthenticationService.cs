6dc17c9374a6c24cae0deda45576d3aecadffea9
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-09 13:25:06 +0800

    Add xml documentation in IAuthentictionService

--- UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Services/AuthenticationService/IAuthenticationService.cs
+++ UserContactInformationManagement/ContactInformation.WebAPI/ContactInformation.WebAPI/Services/AuthenticationService/IAuthenticationService.cs
@@ -2,9 +2,23 @@
 
 namespace ContactInformation.WebAPI.Services.AuthenticationService
 {
+    /// <summary>
+    /// Service interface for user authentication operations.
+    /// </summary>
     public interface IAuthenticationService
     {
+        // <summary>
+        /// Authenticates a user and generates a JWT token.
+        /// </summary>
+        /// <param name="userLoginDto">User login information.</param>
+        /// <returns>JWT token.</returns>
         Task<string> Login(UserLoginDto userLoginDto);
+
+        /// <summary>
+        /// Registers a new user and returns the registered user's data.
+        /// </summary>
+        /// <param name="userRegistrationDto">User registration information.</param>
+        /// <returns>Registered user data.</returns>
         Task<UserDto> Register(UserRegistrationDto userRegistrationDto);
     }
 }
