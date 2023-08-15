156b477018704f380974c4e6d3bb9a768713753b
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-15 13:06:08 +0800

    Add title and logo

---
+++ ContactInformation-Frontend/contactinformation-react/src/main/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div class="w-screen h-screen bg-red-300">
+        <h1 class="text-3xl font-bold underline">Hello World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
