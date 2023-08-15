5bff01bd197036c926c242965b50f131e6c6dbd2
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-15 09:40:17 +0800

    Create fronted vite react app

---
+++ ContactInformation-Frontend/contactinformation-react/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen bg-red-300">
+        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
