702b2889166d89b82ad0109fe0f67e15124e5daa
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-16 19:16:37 +0800

    Add boilerplate pages

--- ContactInformation-Frontend/contactinformation-react/src/pages/contact/ContactListView.jsx
+++ ContactInformation-Frontend/contactinformation-react/src/pages/contact/ContactListView.jsx
@@ -59,39 +59,39 @@ const ContactListView = () => {
     <div>
       <h1 class="text-whiterText">Your Contacts</h1>
 
-      <ul role="list" className="divide-y divide-gray-100">
+      <ul role="list" class="divide-y divide-greyBorder">
         {people.map((person) => (
-          <li key={person.email} className="flex justify-between gap-x-6 py-5">
-            <div className="flex min-w-0 gap-x-4">
+          <li key={person.email} class="flex justify-between gap-x-6 py-5">
+            <div class="flex min-w-0 gap-x-4">
               <img
-                className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                class="h-12 w-12 flex-none rounded-full bg-gray-50"
                 src={person.imageUrl}
                 alt=""
               />
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">
+              <div class="min-w-0 flex-auto">
+                <p class="text-sm font-semibold leading-6 text-whiterText">
                   {person.name}
                 </p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                <p class="mt-1 truncate text-xs leading-5 text-gray-500">
                   {person.email}
                 </p>
               </div>
             </div>
-            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+            <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+              <p class="text-sm leading-6 text-gray-900">{person.role}</p>
               {person.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
+                <p class="mt-1 text-xs leading-5 text-gray-500">
                   Last seen{" "}
                   <time dateTime={person.lastSeenDateTime}>
                     {person.lastSeen}
                   </time>
                 </p>
               ) : (
-                <div className="mt-1 flex items-center gap-x-1.5">
-                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                <div class="mt-1 flex items-center gap-x-1.5">
+                  <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
                   </div>
-                  <p className="text-xs leading-5 text-gray-500">Online</p>
+                  <p class="text-xs leading-5 text-gray-500">Online</p>
                 </div>
               )}
             </div>
