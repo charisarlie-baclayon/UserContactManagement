2fc56bf6a247a7d0fcf9a45503be32c16f28995b
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-16 11:44:27 +0800

    Change LoginView

--- ContactInformation-Frontend/contactinformation-react/src/pages/userauth/LoginView.jsx
+++ ContactInformation-Frontend/contactinformation-react/src/pages/userauth/LoginView.jsx
@@ -20,26 +20,13 @@ const LoginView = () => {
                   name="username"
                   placeholder="username"
                 ></input>
-                <div class="relative">
-                  <input
-                    class="p-2 rounded border w-full"
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                  ></input>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="gray"
-                    class="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                  </svg>
-                </div>
-                <button class="bg-accentPurple rounded text-darkBlack py-2 font-bold">
+                <input
+                  class="p-2 rounded border w-full"
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                ></input>
+                <button class="bg-accentPurple rounded text-darkBlack py-2 font-bold transition duration-300 ease-in-out hover:underline hover:scale-105">
                   Login
                 </button>
               </form>
@@ -52,7 +39,7 @@ const LoginView = () => {
               <p class="text-whiteText text-sm mt-4 text-center pt-2 pb-2">
                 Make an account.
               </p>
-              <button class="my-2 bg-transparent border border-accentPurple rounded text-whiteText py-2 w-full">
+              <button class="my-2 bg-transparent border border-accentPurple rounded text-whiteText py-2 w-full transition duration-300 ease-in-out hover:underline hover:scale-105">
                 Register
               </button>
             </div>
