3c6f175b8398c9afa200f2bc6581e4e5bd0640bb
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-17 18:33:25 +0800

    Change origins program.cs

--- ContactInformation-Backend/ContactInformation.WebAPI/Program.cs
+++ ContactInformation-Backend/ContactInformation.WebAPI/Program.cs
@@ -67,7 +67,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173/")
+                          policy.AllowAnyOrigin()
                                .AllowAnyHeader() // Allow any header
                                .AllowAnyMethod(); // Allow any HTTP method
                       });
