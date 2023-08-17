e7875523d6bbdefd31b65038a33d4976ac0b1fac
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-18 00:30:15 +0800

    Add close when clicked outside

--- ContactInformation-Frontend/contactinformation-react/src/components/Sidebar.jsx
+++ ContactInformation-Frontend/contactinformation-react/src/components/Sidebar.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
+  const sidebarRef = useRef(null);
+
   const Menus = [
     { title: "Contacts", src: "contact.svg", route: "/" },
     { title: "Favorites", src: "favorite.svg", route: "/favorites" },
     { title: "Settings", src: "settings.svg", route: "/settings" },
   ];
+  const handleOutsideClick = (e) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (open) {
+      // Add event listener when the sidebar is open
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      // Remove event listener when the sidebar is closed
+      document.removeEventListener("click", handleOutsideClick);
+    }
+
+    // Clean up event listener on component unmount
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [open]);
+
   return (
     <div
+      ref={sidebarRef}
       className={`${open ? "w-72" : "w-20"} 
       p-5 pt-8 duration-300 bg-mainBlack border-r-2 border-greyBorder sticky top-0 h-screen `}
     >
