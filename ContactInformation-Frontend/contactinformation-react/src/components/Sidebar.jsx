85e7340be25d6aca6fdbf490b3610500f38e44a3
Charis Arlie Baclayon <cbaclayon@fullscale.io>
2023-08-17 17:57:21 +0800

    Update namings

--- ContactInformation-Frontend/contactinformation-react/src/components/Sidebar.jsx
+++ ContactInformation-Frontend/contactinformation-react/src/components/Sidebar.jsx
@@ -11,41 +11,43 @@ const Sidebar = () => {
   ];
   return (
     <div
-      class={`${open ? "w-72" : "w-20"} 
+      className={`${open ? "w-72" : "w-20"} 
       p-5 pt-8 duration-300 bg-mainBlack border-r-2 border-greyBorder sticky top-0 h-screen `}
     >
       <div
-        class="flex gap-x-4 items-center cursor-pointer"
+        className="flex gap-x-4 items-center cursor-pointer"
         onClick={() => setOpen(!open)}
       >
         <img
           src="/contactlogo.png"
-          class={`w-8 cursor-pointer duration-500 ${open && "rotate-[360deg]"}`}
+          className={`w-8 cursor-pointer duration-500 ${
+            open && "rotate-[360deg]"
+          }`}
         ></img>
         <h1
-          class={`text-whiterText origin-left font-medium text-2xl duration-300 ${
+          className={`text-whiterText origin-left font-medium text-2xl duration-300 ${
             !open && "scale-0"
           }`}
         >
           Contact
         </h1>
       </div>
-      <div class="pt-4">
+      <div className="pt-4">
         <ul>
           {Menus.map((menu, index) => (
             <li
               key={index}
-              className={`text-whiterText text-sm flex items-center gap-x-4 pt-4 pb-4 pl-1 cursor-pointer 
+              classNameName={`text-whiterText text-sm flex items-center gap-x-4 pt-4 pb-4 pl-1 cursor-pointer 
       hover:bg-accentPurple rounded-md`}
               onClick={() => navigate(menu.route)}
             >
               <img
                 src={`./src/assets/${menu.src}`}
-                className="w-8"
+                classNameName="w-8"
                 alt={menu.title}
               />
               <span
-                className={`${
+                classNameName={`${
                   !open && "hidden"
                 } origin-left duration-500 text-xl`}
               >
